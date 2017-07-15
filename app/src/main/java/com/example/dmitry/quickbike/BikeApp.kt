ea368679b6774f7ddbd05cb0094351213ce013c6
A.Misiuilia <a.misiulia@ezrapplication.com>
2017-07-15 17:52:49 +0300

    [REF] Db entities, add filling during onCreate application

--- app/src/main/java/com/example/dmitry/quickbike/BikeApp.kt
+++ app/src/main/java/com/example/dmitry/quickbike/BikeApp.kt
@@ -4,17 +4,25 @@ import android.app.Application
 import com.example.dmitry.quickbike.di.AppComponent
 import com.example.dmitry.quickbike.di.AppModule
 import com.example.dmitry.quickbike.di.DaggerAppComponent
+import com.example.dmitry.quickbike.utils.fillContent
+import com.example.dmitry.quickbike.utils.isEmpty
 
 class BikeApp : Application() {
     companion object {
-        lateinit var instance : BikeApp
-        lateinit var  appComponent: AppComponent
+        lateinit var instance: BikeApp
+        lateinit var appComponent: AppComponent
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         appComponent = createComponent()
+        val db = appComponent.getDb()
+        db.isEmpty()
+                .filter { isEmpty -> isEmpty }
+                .flatMapCompletable { db.fillContent() }
+                .subscribe()
+
     }
 
     private fun createComponent(): AppComponent {
